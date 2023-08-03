7b1a9ffa7ef5fdc30a73b565b188859a61dcfab0
Reggie Wlodarczyk <r.wlodarczyK@embloom.com>
2023-08-03 12:48:21 +0200

    initial commit

---
+++ zap.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'zaplib.php';
+
+// open db connection
+$db = new mysqli('localhost', 'root', '');
+$db->select_db('zap');
+
+// inject db
+$zap = Zap::instance();
+$zap->setDb($db);
+
+// read querystring 
+$where = (isset($_GET['r']) ? $zap->decodePeriods($_GET['r']) : false);
+$who = !array_key_exists('zapper', $_COOKIE) ? false : intval($_COOKIE['zapper']);
+$listing = isset($_GET['l']);
+
+if (array_key_exists('submit', $_POST)) {
+	$who = $zap->storeZapperInCookie($_POST['zapper']);
+}
+
+if ($who && false !== $where) {
+	$zap->storeZap($where, $who);
+}
+
+if ($who && !$listing) {
+	// the following call issues a header redirect:
+	$zap->loadZap();
+}
+
+// OR:
+?>
+
+<html>
+<head>
+	<title>Zap</title>
+	<link rel="stylesheet" type="text/css" href="zap.css">
+</head>
+<body>
+	<?php echo (false === $who ? $zap->getUserFormHtml() : '')?>
+	Zaps:<br>
+	<br>
+	<?php echo $zap->getZapListingHtml();?>
+</body>
+</html>
+
+<?php
+mysqli_close($db);
---
+++ zaplib.php
@@ -0,0 +1,106 @@
+<?php
+
+class Zap 
+{
+	private static Zap $instance;
+	
+	private mysqli $db;
+	
+	public function setDb(mysqli $db) 
+	{
+		$this->db = $db;
+	}
+	
+	protected function __construct() { }
+	
+	public static function instance(): Zap
+	{
+		if (!isset(self::$instance)) {
+			self::$instance = new Zap;
+		}
+		
+		return self::$instance;
+	}
+		
+	public function storeZapperInCookie(int $zapperid): int
+	{
+		$retval = null;
+
+		if (null !== $zapperid) {
+			$result = $this->db->query(sprintf("SELECT * FROM zappers WHERE id = %d", intval($zapperid)));
+
+			if ($row = mysqli_fetch_assoc($result)) {
+				setcookie('zapper', $zapperid, time() + 60 * 60 * 24 * 365, '/');
+				$retval = $zapperid;
+			}
+		}
+
+		return $retval;
+	}
+		
+	public function decodePeriods(string $where): string
+	{    
+		return str_replace('%PX', '.', $where);
+	}
+		
+	public function storeZap(string $url, int $zapperid, mysqli $db)
+	{
+		$this->db->query(sprintf("INSERT INTO zaps (id, url, userid) VALUES(NULL, '%s', %d)", mysqli_real_escape_string($db, $url), intval($zapperid)));
+	}
+		
+	public function getUserFormHtml(): string
+	{
+		$zappers = array();
+
+		$result = $this->db->query("SELECT id, name FROM zappers ORDER BY name ASC");
+
+		while($row = mysqli_fetch_assoc($result))
+		{
+			$zappers[] = array('id' => $row['id'], 'name' => $row['name']);
+		}
+
+		ob_start();
+		?>
+		<form action="zap.php" method="post">
+			<select name="zapper">
+				<?php echo array_walk($zappers, function($value, $index) {
+					echo sprintf("<option value=\"%s\">%s</option>", $value['id'], $value['name']);
+				})?>
+			</select>
+			<button name="submit" type="submit">Opslaan</button>
+		</form>
+		<?php
+
+		return ob_get_clean();
+	}
+		
+	public function getZapListingHtml(): string
+	{
+		$result = $this->db->query("SELECT z.id, z.url, zn.name, z.timestamp FROM zaps z, zappers zn WHERE z.userid = zn.id ORDER BY timestamp DESC");
+		$lis = array();
+
+		while ($row = mysqli_fetch_assoc($result)) {
+			$lis[] = sprintf("<li><a href=\"%s\">%s</a><br><small>%s</small> by %s</li>", $row['url'], $row['url'], date('Y-m-d @ h:i', strtotime($row['timestamp'])), $row['name']);
+		}
+
+		return sprintf("<ul>%s</ul>", implode('', $lis));
+	}
+
+	/**
+	 * Issues a header redirect.
+	 */
+	public function loadZap(): void
+	{	
+		$result = $this->db->query("SELECT url FROM zaps ORDER BY timestamp DESC LIMIT 1");
+		
+		if ($row = mysqli_fetch_assoc($result)) { 
+			// close the database connection; we are leaving the page
+			$this->db->close();
+			
+			   var_dump($row);
+			header(sprintf("Location: %s", $row['url']));
+			exit;
+		}
+	}
+}
+
